9681276328914da1f404b6b18759c4560b9ef422
legoyb <bastien.legoy@grenoble-inp.org>
2022-11-25 17:36:13 +0100

    Input Piano Keyboard

--- Assets/Scripts/Tests/PianoInput.cs
+++ Assets/Scripts/Tests/PianoInput.cs
@@ -1,19 +1,48 @@
 using MidiJack;
+using MidiPlayerTK;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.ARFoundation;
 
 public class PianoInput : MonoBehaviour
 {
+    public MidiStreamPlayer MidiStream;
+
     // Start is called before the first frame update
     void Start()
     {
         MidiMaster.noteOnDelegate = onNoteOn;
+        MidiMaster.noteOffDelegate = onNoteOff;
+    }
+
+    private int noteToKeyboard(int note)
+    {
+        return note - 12;
     }
 
     private void onNoteOn(MidiChannel channel, int note, float velocity)
     {
         Debug.Log($"CH:{channel} N:{note} V:{velocity}");
+
+        MidiStream.MPTK_PlayEvent(new MPTKEvent()
+        {
+            Command = MPTKCommand.NoteOn,
+            Channel = (int)channel,
+            Velocity = (int)(velocity * 127),
+            Value = noteToKeyboard(note)
+        });
+    }
+
+    private void onNoteOff(MidiChannel channel, int note)
+    {
+        MidiStream.MPTK_PlayEvent(new MPTKEvent()
+        {
+            Command = MPTKCommand.NoteOff,
+            Channel = (int)channel,
+            Value = noteToKeyboard(note)
+        });
     }
 
     // Update is called once per frame
